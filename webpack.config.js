f0a594cd8d45540d341f4fa1e161bac4b154ec44
okayurisotto <aytkzm@gmail.com>
2021-03-02 20:31:45 +0900

    update: webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+    ],
+  },
   target: 'node',
 };
